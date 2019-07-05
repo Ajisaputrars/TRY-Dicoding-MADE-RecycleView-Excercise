87dcf2021ce8028110705f004a69fdfcbc3a8317
Ajisaputrars <Ajisaputrars@hotmail.com>
2019-07-05 20:32:12 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/ajisaputrars/recycleviewapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ajisaputrars.recycleviewapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvCategory;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvCategory = findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list.addAll(HeroData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(this);
+        listHeroAdapter.setListHero(list);
+        rvCategory.setAdapter(listHeroAdapter);
+    }
+}
